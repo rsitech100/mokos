35ffeb08899c363d58753862fede47b812751ed6
Hilmy <hilmyghilmani@gmail.com>
2025-12-03 15:10:14 +0700

    fix: image hero

--- src/containers/home/hero-section/index.tsx
+++ src/containers/home/hero-section/index.tsx
@@ -7,14 +7,14 @@ import Image from "next/image";
 import { ImagesType } from "@/types/images";
 
 const ImagesItem: ImagesType[] = [
-      { image: '/image/home/hero-1.svg', alt: 'banner-hero-1' },
-      { image: '/image/home/hero-2.svg', alt: 'banner-hero-2' },
-      { image: '/image/home/hero-3.svg', alt: 'banner-hero-3' },
+      { image: '/image/home/hero-1-rev.jpg', alt: 'banner-hero-1' },
+      { image: '/image/home/hero-2-rev.jpg', alt: 'banner-hero-2' },
+      { image: '/image/home/hero-3-rev.jpg', alt: 'banner-hero-3' },
 ]
 
 export function Hero() {
       return (
-            <section className="px-5 lg:px-20 w-full h-full sm:h-fit lg:h-[300px] flex items-center justify-center">
+            <section className="px-5 lg:px-20 w-full h-fullsm:h-fit lg:h-[300px] flex items-center justify-center">
                   <Swiper
                         modules={[Autoplay, Pagination]}
                         pagination={{ clickable: true }}
@@ -26,13 +26,13 @@ export function Hero() {
                         centeredSlides={true}
                   >
                         {ImagesItem.map((item) => (
-                              <SwiperSlide key={item.alt} className="w-full !h-full object-contain">
+                              <SwiperSlide key={item.alt} className="w-full max-h-[350px] flex items-center justify-center">
                                     <Image
                                           src={item.image}
                                           alt={item.alt}
                                           width={100}
                                           height={100}
-                                          className="rounded-2xl w-full h-full object-contain"
+                                          className="rounded-2xl w-full h-full object-cover"
                                           loading="lazy"
                                     />
                               </SwiperSlide>
